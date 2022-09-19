51a5ad405c56356d9d983de8f9584234fcb1211a
mirzaskola <mirza.kurtovic@stu.ibu.edu.ba>
2022-09-20 01:09:43 +0200

    Attempt at fixing CORS error

--- rest/routes/ApiSpec.php
+++ rest/routes/ApiSpec.php
@@ -4,7 +4,7 @@
  * @OA\Info(title="Gamers Paradise API Specs", version="0.2", @OA\Contact(email="mirza.kurtovic@stu.ibu.edu.ba", name="Mirza Kurtovic"))
  * @OA\OpenApi(
  *    @OA\Server(url="http://localhost/WebProgramiranje2022/rest", description="Development Environment" ),
- *    @OA\Server(url="http://webprogramiranje2022.herokuapp.com/rest", description="Production Environment" )
+ *    @OA\Server(url="https://webprogramiranje2022.herokuapp.com/rest", description="Production Environment" )
  * ),
  * @OA\SecurityScheme(securityScheme="ApiKeyAuth", type="apiKey", in="header", name="Authorization" )
  */
